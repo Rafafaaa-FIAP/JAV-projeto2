c8dd3b0d7a6c8dc3f9496580a9ab5608b1dd4064
Rafafaaa <cristofalirafael@gmail.com>
2024-09-10 20:08:47 -0300

    first commit

---
+++ src/br/com/fiap/portal/teste/Teste.java
@@ -0,0 +1,14 @@
+package br.com.fiap.portal.teste;
+
+import br.com.fiap.portal.model.Aluno;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Aluno aluno = new Aluno();
+		
+		aluno.setNome("Teste");
+		aluno.setIdade(10);
+	}
+
+}
